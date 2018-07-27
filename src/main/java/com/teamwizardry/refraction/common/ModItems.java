11b253d08900b180f932c74b71b1997ac816dd01
Shyrik <r.wesenberg@gmail.com>
2018-07-27 22:24:28 +0200

    added item laser pen
    no cat chasing posible without entity :(

--- src/main/java/com/teamwizardry/refraction/common/ModItems.java
+++ src/main/java/com/teamwizardry/refraction/common/ModItems.java
@@ -1,12 +1,15 @@
 package com.teamwizardry.refraction.common;
 
+import com.teamwizardry.refraction.common.item.ItemLaserPen;
 import com.teamwizardry.refraction.common.item.ItemScrewDriver;
 
 public class ModItems {
 
 	public static ItemScrewDriver SCREWDRIVER;
+	public static ItemLaserPen LASERPEN;
 
 	public static void init() {
 		SCREWDRIVER = new ItemScrewDriver();
+		LASERPEN = new ItemLaserPen();
 	}
 }
---
+++ src/main/java/com/teamwizardry/refraction/common/item/ItemLaserPen.java
@@ -0,0 +1,69 @@
+package com.teamwizardry.refraction.common.item;
+
+import com.teamwizardry.librarianlib.features.base.item.ItemMod;
+import com.teamwizardry.refraction.api.Beam;
+import com.teamwizardry.refraction.api.beam.Beam;
+import com.teamwizardry.refraction.api.utils.Utils;
+import com.teamwizardry.refraction.common.effect.EffectAesthetic;
+import com.teamwizardry.refraction.common.entity.EntityLaserPointer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumHandSide;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import java.awt.*;
+
+/**
+ * Created by Demoniaque
+ */
+public class ItemLaserPen extends ItemMod {
+
+	public static final double RANGE = 32;
+
+	public ItemLaserPen() {
+		super("laser_pen");
+		setMaxStackSize(1);
+	}
+
+	@Override
+	@Nonnull
+	public EnumAction getItemUseAction(ItemStack stack) {
+		return EnumAction.BOW;
+	}
+
+	@Override
+	public int getMaxItemUseDuration(ItemStack stack) {
+		return 1000;
+	}
+
+	@Nonnull
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, @Nonnull EnumHand hand) {
+		ItemStack itemStackIn = playerIn.getHeldItem(hand);
+		playerIn.setActiveHand(hand);
+		if (!worldIn.isRemote) {
+			worldIn.spawnEntity(e);
+		}
+		return new ActionResult<>(EnumActionResult.SUCCESS, itemStackIn);
+	}
+
+	@Override
+	public void onUsingTick(ItemStack stack, EntityLivingBase player, int count) {
+		boolean handMod = player.getHeldItemMainhand() == stack ^ player.getPrimaryHand() == EnumHandSide.LEFT;
+
+		Vec3d cross = player.getLook(1).crossProduct(new Vec3d(0, player.getEyeHeight(), 0)).normalize().scale(player.width / 2);
+		if (!handMod) cross = cross.scale(-1);
+		Vec3d playerVec = new Vec3d(player.posX + cross.x, player.posY + player.getEyeHeight() + cross.y, player.posZ + cross.z);
+
+		new Beam(playerVec, player.getLook(1), RANGE, new Color(0x20FF0000, true), Utils.createUUID(player.getName()))
+				.addEntityToSkip(player.getUniqueID())
+				.spawn(player.getEntityWorld());
+	}
+}
